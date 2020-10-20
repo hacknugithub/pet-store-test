9560cdf8ff2afbc0e2188464feed914f43be207e
Manuel Guerrero <manu_pacu@hotmail.com>
2020-10-20 13:35:55 -0500

    Basic scaffolding, sqlite test connection

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Pet Store API - Test
+
+## Configuring Database driver:
+
+In this proyect we are using **sequelize** as the ORM.
+
+### Choose the database
+
+Depending on the database you are going to use, open a terminal and run:
+
+- npm install --save pg pg-hstore _Postgres_
+- npm install --save mysql2
+- npm install --save mariadb
+- npm install --save sqlite3
+- npm install --save tedious _Microsoft SQL Server_
---
+++ db/index.js
@@ -0,0 +1,19 @@
+const {Sequelize} = require('sequelize');
+
+// Conection demo for sqlite
+// TODO: Consider implementing dinamic assigment of database with parameters as driver option
+export const sequelize = () => {
+    const connection = new Sequelize({
+        dialect: 'sqlite',
+        storage: './petstore.sqlite',
+    });
+    try {
+        await connection.authenticate();
+        console.log({success: true, message: "Connection has been established successfully"});
+        return connection;
+    } catch(error){
+        return {success: false, message: error.message}
+    }
+} 
+
+export default sequelize;
